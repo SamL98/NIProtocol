2f747878f0307a93c1393c7ff4d5cd6e60638525
Sam Lerner <lerner98@gmail.com>
2019-08-09 13:59:28 -0400

    Refactored parser to be just c++

---
+++ parser/parser.cpp
@@ -0,0 +1,86 @@
+#include "parser.h"
+#include <stdio.h>
+
+uint32_t
+btn_code_to_btn_num(uint32_t code)
+{
+	uint32_t r,c;
+	
+	r = code / 4;
+	c = code % 4;
+
+	return (3 - r) * 4 + c + 1;
+}
+
+void 
+parse_wheel_msg(uint32_t *data,
+				mk2_msg *msg)
+{
+	msg->type = WheelType;
+	msg->msg.wheel_msg.btn = *data;
+	msg->msg.wheel_msg.state = *(data+4);
+}
+
+void
+parse_button_msg(uint32_t *data,
+				 mk2_msg *msg)
+{
+	msg->type = ButtonType;
+	msg->msg.button_msg.btn = btn_code_to_btn_num(*data);
+	msg->msg.button_msg.pressure = *(data+8);
+}
+
+mk2_msg *
+parse_packet(char *packet, 
+			 size_t packetLen,
+			 size_t *nmsgs)
+{
+	uint32_t *fields,
+			 ctrl;
+	size_t 	 msgLen,
+			 msgStructLen,
+			 i,
+			 packetMsgsLen;
+	void	 (*parse_fn)(uint32_t*, mk2_msg*);
+	mk2_msg  *msgs;
+	MsgType  msgType;
+
+	msgs = NULL;
+	*nmsgs = 0;
+
+	fields = (uint32_t *)packet;
+	ctrl = *fields;
+	fields += 4;
+	packetMsgsLen = packetLen - 4;
+
+	if (ctrl == kWheelCtrl) {
+		msgType = WheelType;
+		msgLen = kWheelMsgLen;
+		msgStructLen = sizeof(mk2_wheel_msg);
+		parse_fn = &parse_wheel_msg;
+	}
+	else if (ctrl == kButtonsCtrl) {
+		msgType = ButtonType;
+		msgLen = kButtonMsgLen;
+		msgStructLen = sizeof(mk2_button_msg);
+		parse_fn = &parse_button_msg;
+	}
+	else {
+		printf("Unrecognized control code: %x", ctrl);
+		return msgs;
+	}
+
+	*nmsgs = packetMsgsLen / msgLen;
+
+	if (!(msgs = (mk2_msg *)malloc(msgStructLen * (*nmsgs)))) {
+		printf("Couldn't malloc %lu msgs\n", *nmsgs);
+		return msgs;
+	}
+
+	for (i=0; i<(*nmsgs); i++) {
+		parse_fn(fields, &msgs[i]);
+		fields += msgLen;
+	}
+
+	return msgs;
+}
---
+++ parser/parser.h
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#define kWheelCtrl 0x7703
+#define kButtonsCtrl 0x5003
+#define kWheelMsgLen 8
+#define kButtonMsgLen 12
+
+enum MsgType { WheelType, ButtonType };
+
+typedef struct {
+	uint32_t btn;
+	int32_t  state;
+} mk2_wheel_msg;
+
+typedef struct {
+	uint32_t btn;
+	uint32_t pressure;
+} mk2_button_msg;
+
+typedef struct {
+	enum MsgType type;
+	union {
+		mk2_button_msg button_msg;
+		mk2_wheel_msg  wheel_msg;
+	} msg;
+} mk2_msg;
+
+mk2_msg *parse_packet(char *packet, size_t packetLen, size_t *nmsgs);
+void free_msgs(mk2_msg *msgs);
